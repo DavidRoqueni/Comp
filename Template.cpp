d4ee5f8367a11a7093ed1e48f22fa7490d2612b4
David Roqueni <45717673+DavidRoqueni@users.noreply.github.com>
2021-11-22 21:20:26 -0700

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<string,string> pss;
+//typedef vector<int> vt;
+//typedef vector<vt> vtt;
+//typedef vector<pii> vpi;
+//typedef vector<ll> vl;
+//typedef vector<vl> vvl;
+#define FOR(a,b) for((int)a=0; (a)<(b);++(a)
+#define FOR(a,b,c) for((int)a=(b); (a)<(c);++(a)
+#define FOREACH(a,b) for(auto&(a):(b))
+#define MAX(a,b) a=max(a,b)
+#define MIN(a,b) a=min(a,b)
+#define SQR(x) ((ll)(x)*(x))
+#define pb push_back
+#define ALL(v) v.begin(), v.end()
+#define SIZE(v) (int)v.size()
+#define SORT(v) sort(ALL(V))
+
+int main(){
+
+}
